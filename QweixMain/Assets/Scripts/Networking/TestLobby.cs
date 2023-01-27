c0165cdb0e0ea771b2a13ad520d1f0cd36c1d64b
Gavin-Cunningham <120994965+Gavin-Cunningham@users.noreply.github.com>
2023-01-27 11:17:22 -0800

    Started Lobbies Tutorial
    -Imported Lobbies Package
    
    -Began TestLobby script off of CodeMonkey Tutorial.

---
+++ QweixMain/Assets/Scripts/Networking/TestLobby.cs
@@ -0,0 +1,86 @@
+using QFSW.QC;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.Services.Authentication;
+using Unity.Services.Core;
+using Unity.Services.Lobbies;
+using Unity.Services.Lobbies.Models;
+using UnityEngine;
+
+public class TestLobby : MonoBehaviour
+{
+
+    private Lobby hostLobby;
+    private float heartbeatTimer;
+
+    //A function must be marked as await to prevent the program freezing while waiting for a response over the internet.
+    //Any method with an awaited function in it must also be marked as async
+    private async void Start()
+    {
+        await UnityServices.InitializeAsync();
+
+        AuthenticationService.Instance.SignedIn += () =>
+        {
+            Debug.Log("Signed in " + AuthenticationService.Instance.PlayerId);
+        };
+        //Creates an anonymous sign in for the player. Can be replaced with sign in requests from things like Steam or Google play instead
+        await AuthenticationService.Instance.SignInAnonymouslyAsync();
+    }
+
+    private void Update()
+    {
+        HandleLobbyHeartbeat();
+    }
+
+    private async void HandleLobbyHeartbeat()
+    {
+        if (hostLobby != null)
+        {
+            heartbeatTimer -= Time.deltaTime;
+            if (heartbeatTimer < 0f)
+            {
+                float heartbeatTimerMax = 15;
+                heartbeatTimer = heartbeatTimerMax;
+
+                await LobbyService.Instance.SendHeartbeatPingAsync(hostLobby.Id);
+            }
+        }
+    }
+
+    //[Command] is used by the Quantum Console (QFSW.QC) to identify functions that can be called in the console.
+    [Command]
+    private async void CreateLobby()
+    {
+        try
+        {
+            string lobbyName = "MyLobby";
+            int maxPlayers = 4;
+            Lobby lobby = await LobbyService.Instance.CreateLobbyAsync(lobbyName, maxPlayers);
+
+            hostLobby = lobby;
+
+            Debug.Log("Created Lobby! " + lobby.Name + " " + lobby.MaxPlayers);
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+
+    [Command]
+    private async void ListLobbies()
+    {
+        try
+        {
+            QueryResponse queryResponse = await Lobbies.Instance.QueryLobbiesAsync();
+
+            Debug.Log("Lobbies found: " + queryResponse.Results.Count);
+            foreach (Lobby lobby in queryResponse.Results)
+            {
+                Debug.Log(lobby.Name + " " + lobby.MaxPlayers);
+            }
+        } catch (LobbyServiceException e)
+        {
+            Debug.Log(e);
+        }
+    }
+}
